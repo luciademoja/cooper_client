f0fe05239e6f2ece46c79c08307066fa0c33ff51
Lucia De Mojà <luciademoja@gmail.com>
2016-08-16 09:38:12 +0200

    install ng-token-auth load ng-token-auth and angular-cookie to index.html added ng-token-auth to app.js updated API_URL on app.js configured API_URL

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,11 @@
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ng-token-auth'])
+    .constant('API_URL', 'https://ldm-cooper-api.herokuapp.com/api/v1/auth')
+
+  .config(function ($authProvider, API_URL) {
+    $authProvider.configure({
+      apiUrl: API_URL
+    });
+  })
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
